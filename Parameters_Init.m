e543b0171d463a39220dd3d027049c8902b9852e
Hoang <chukhanhhoang97@gmail.com>
2021-05-11 11:37:22 +0200

    add offset

--- Parameters_Init.m
+++ Parameters_Init.m
@@ -3,7 +3,7 @@ clc; clear; close all;
 %% Define Parameters of the system
 m_m = 0.118; m_p = 0.682; m = 1/3*m_p+m_m; %TODO: Fix m_m and m_p
 K = 11; g = 9.81; Cv = 16.5;
-Zc = 0.352;
+Zc = 0.32;
 
 %% Define transfer function of Motor
 numerator = [K];
