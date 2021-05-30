f55d7b7e3aea86ded92ed2b428342702a46687ef
Sophistt <wang.canye@gmail.com>
2021-05-30 12:32:44 +0200

    re-arrange files

--- plateToBallLinear.m
+++
@@ -1,9 +0,0 @@
-function ydot = plateToBallLinear(t, y, theta_1, theta_2, I_b, m_b, R_b)
-    % y = [x, y, xdot, ydot]
-    % ydot = [xdot, ydot, xddot, yddot]
-    g = 9.81;
-
-    ydot = [y(3:4);
-            theta_1 * g/(1+I_b/(m_b*R_b^2));
-            theta_2 * g/(1+I_b/(m_b*R_b^2))];
-end
