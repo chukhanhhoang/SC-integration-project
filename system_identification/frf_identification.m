e707d9eac6093221a8d9a6121f74f695ca8a85aa
Sophistt <wang.canye@gmail.com>
2021-06-15 11:23:34 +0200

    Temp

--- system_identification/frf_identification.m
+++ system_identification/frf_identification.m
@@ -1,12 +1,14 @@
 clc; clear; close all;
 addpath(genpath(pwd)); % Add files;
 
-load('FRF_0511_1.mat');
+load('data0416_3points_ballplate.mat');
 
 % Variables assign
-u = FRF_0511_1.Y(4).Data;
-d = FRF_0511_1.Y(3).Data;
-r = FRF_0511_1.Y(2).Data;
-P = FRF_0511_1.Y(1).Data;
+u_th1 = data0416_3points.Y(3).Data;
+u_th2 = data0416_3points.Y(4).Data;
 
-plot(P);
+r_bX  = data0416_3points.Y(5).Data;
+r_bY  = data0416_3points.Y(6).Data;
+
+e_bX  = data0416_3points.Y(1).Data;
+e_bY  = data0416_3points.Y(2).Data;
